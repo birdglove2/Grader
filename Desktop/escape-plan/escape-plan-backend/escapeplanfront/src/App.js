8fedf81b2e5a2c1faaad8326478b0bd090ac1b85
Krittamook Aksornchindarat <krittamook@hotmail.com>
2020-11-02 11:22:33 +0700

    Add new front

---
+++ Desktop/escape-plan/escape-plan-backend/escapeplanfront/src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import HowToPlay from "./pages/HowToPlay";
+import ServerOrClient from "./pages/ServerOrClient";
+import GameArea from "./pages/GameArea";
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={HomePage}></Route>
+          <Route path="/gamearea" exact component={GameArea}></Route>
+          <Route
+            path="/serverorclient"
+            exact
+            component={ServerOrClient}
+          ></Route>
+          <Route path="/howtoplay" exact component={HowToPlay}></Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
